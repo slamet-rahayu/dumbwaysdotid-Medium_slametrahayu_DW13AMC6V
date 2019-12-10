4fd659807ae9ec92450e1ad4c84ffe555fe745e6
slamet-rahayu <slamet_rahayu93@yahoo.com>
2019-12-10 11:04:16 +0700

    5.Category-page

--- src/view/Category-page.js
+++ src/view/Category-page.js
@@ -6,6 +6,9 @@ import Col from 'react-bootstrap/Col';
 import Medium from '../medium.png';
 import One from '../onezero.PNG';
 import '../App.css';
+import Card from 'react-bootstrap/Card';
+import Pixel from '../pixel.gif';
+import Ps from '../ps.PNG';
 
 function Home () {
 return (
@@ -17,13 +20,11 @@ return (
     <Nav className="mr-auto">
 
     </Nav>
-    <Form inline>
       <span style={{fontSize:"20px",marginRight:"10px"}}>&#128269;</span>  
       <span style={{fontSize:"25px",marginRight:"10px"}}>&#128365;</span>  
       <Button variant="outline-dark btn-sm">Upgrade</Button>
       &nbsp;&nbsp;&nbsp;
       <div className="profile"><p style={{marginTop:"-10px"}}></p>R</div>
-    </Form>
   </Navbar.Collapse>
 </Navbar>
 
@@ -92,18 +93,129 @@ return (
 <Row>
     <Col>
     <div className="box1">
-        <div>
-            <p style={{fontSize:"30px",textAlign:"center"}}>"Would you pay $1 million and fly to South America for a chance to live longer?”</p>
-            <p style={{}}>-Emily Mullin</p>
+        <div className="text">
+            <p style={{fontSize:"30px"}}>"Would you pay $1 million and fly to South America for a chance to live longer?”</p>
+            <p>-Emily Mullin</p>
+            <p>Alexa Is My Problematic Fave</p>
+            <p>Dec 9 . 6 mins read</p>
         </div>
     </div>
     </Col>
 </Row>
+<br></br><br></br>
+<Row>
+    <Col>
+    <img src={Pixel} alt="pixel" style={{width:"100%"}}></img>
+    </Col>
+</Row>
+<br></br><br></br>
+<Row>
+    <Col style={{background:"purple"}}>
+    <img src={One} alt="one" style={{marginLeft:"-2%",float:"left"}}></img>
+    <div>
+        <p style={{color:"white",fontSize:"30px",textAlign:"center",marginTop:"5%"}}>The Forgotten Pixel Art Masterpieces of the PlayStation 1 Era</p>
+        <p style={{color:"white",textAlign:"center",fontSize:"18px",marginTop:"-1%"}}>Video games simply can't made like this anymore</p>
+        <p style={{color:"yellow",textAlign:"center"}}>Richmond Lee</p>
+        <p style={{textAlign:"center",fontWeight:"bold",color:"white",marginTop:"-2%"}}>Dec 7 . 9 mins read</p>
+    </div>
+    </Col>
+</Row>
+<br></br><br></br>
+<Row>
+    <Col>
+    <img src={Pixel} alt="pixel" style={{width:"100%"}}></img>
+    </Col>
+</Row>
+<Row>
+    <Col></Col>
+    <Col lg={8}>
+        <hr></hr>
+    <Row>
+        <Col lg={9}>
+        <p style={{fontSize:"20px"}}>How the PlayStation Took Over the World</p>
+        <p style={{fontSize:"18px",display:"inline"}}>Will the strategies that have powered Sony’s success in gaming for 25 years translate to the streaming generation?</p>
+        </Col>
+        <Col lg={3}>
+        <img src={Ps} alt="Ps"></img>
+        </Col>
+    </Row>
+    <hr></hr>
+    <Row>
+        <Col lg={9}>
+        <p style={{fontSize:"20px"}}>How the PlayStation Took Over the World</p>
+        <p style={{fontSize:"18px",display:"inline"}}>Will the strategies that have powered Sony’s success in gaming for 25 years translate to the streaming generation?</p>
+        </Col>
+        <Col lg={3}>
+        <img src={Ps} alt="Ps"></img>
+        </Col>
+    </Row>
+    <hr></hr>
+    <Row>
+        <Col lg={9}>
+        <p style={{fontSize:"20px"}}>How the PlayStation Took Over the World</p>
+        <p style={{fontSize:"18px",display:"inline"}}>Will the strategies that have powered Sony’s success in gaming for 25 years translate to the streaming generation?</p>
+        </Col>
+        <Col lg={3}>
+        <img src={Ps} alt="Ps"></img>
+        </Col>
+    </Row>
+    <hr></hr>
+    <Row>
+        <Col lg={9}>
+        <p style={{fontSize:"20px"}}>How the PlayStation Took Over the World</p>
+        <p style={{fontSize:"18px",display:"inline"}}>Will the strategies that have powered Sony’s success in gaming for 25 years translate to the streaming generation?</p>
+        </Col>
+        <Col lg={3}>
+        <img src={Ps} alt="Ps"></img>
+        </Col>
+    </Row>
+    <hr></hr>
+    <Row>
+        <Col lg={9}>
+        <p style={{fontSize:"20px"}}>How the PlayStation Took Over the World</p>
+        <p style={{fontSize:"18px",display:"inline"}}>Will the strategies that have powered Sony’s success in gaming for 25 years translate to the streaming generation?</p>
+        </Col>
+        <Col lg={3}>
+        <img src={Ps} alt="Ps"></img>
+        </Col>
+    </Row>
+    <hr></hr>
+    <Row>
+        <Col lg={9}>
+        <p style={{fontSize:"20px"}}>How the PlayStation Took Over the World</p>
+        <p style={{fontSize:"18px",display:"inline"}}>Will the strategies that have powered Sony’s success in gaming for 25 years translate to the streaming generation?</p>
+        </Col>
+        <Col lg={3}>
+        <img src={Ps} alt="Ps"></img>
+        </Col>
+    </Row>
+    <hr></hr>
+    <Row>
+        <Col lg={9}>
+        <p style={{fontSize:"20px"}}>How the PlayStation Took Over the World</p>
+        <p style={{fontSize:"18px",display:"inline"}}>Will the strategies that have powered Sony’s success in gaming for 25 years translate to the streaming generation?</p>
+        </Col>
+        <Col lg={3}>
+        <img src={Ps} alt="Ps"></img>
+        </Col>
+    </Row>
+    <hr></hr>
+    <Row>
+        <Col lg={9}>
+        <p style={{fontSize:"20px"}}>How the PlayStation Took Over the World</p>
+        <p style={{fontSize:"18px",display:"inline"}}>Will the strategies that have powered Sony’s success in gaming for 25 years translate to the streaming generation?</p>
+        </Col>
+        <Col lg={3}>
+        <img src={Ps} alt="Ps"></img>
+        </Col>
+    </Row>
+    </Col>
+    <Col></Col>
+</Row>
 
+<hr></hr>
+<p style={{textAlign:"right"}}>About OneZero · About Medium · Terms · Privacy</p>
 </Container>
-
-
-
 );
 }
 
