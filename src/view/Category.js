5a1d57d8aaeabdb45fba4a304cbc016ac699ff73
slamet-rahayu <slamet_rahayu93@yahoo.com>
2019-12-11 08:50:04 +0700

    4.Category

--- src/view/Category.js
+++ src/view/Category.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel'
 import '../App.css';
+import Cat from '../json/category';
 
 
 function Category () {
@@ -10,18 +11,7 @@ return (
     <table cellPadding="12">
     <tr>
         <td>&lt;</td>
-        <td>HOME</td>
-        <td>ONEZERO</td>
-        <td>ELEMENTAL</td>
-        <td>GEN</td>
-        <td>ZORA</td>
-        <td>FORGE</td>
-        <td>HUMAN PARTS</td>
-        <td>MARKER</td>
-        <td>LEVEL</td>
-        <td>HEATED</td>
-        <td>MODUS</td>
-        <td>MORE</td>
+        {Cat.Category.map(s => (<td>{s}</td>))}
         <td>&gt;</td>
     </tr>
 </table>
